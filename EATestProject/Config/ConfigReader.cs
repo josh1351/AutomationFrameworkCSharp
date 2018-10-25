7e2fb46c23b15daac8a5d31e6d7a8dbdd9bd5da3
josh1351 <josh.adal.rios@gmail.com>
2018-10-25 14:21:06 -0500

    first commit

---
+++ EATestProject/Config/ConfigReader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Xml;
+using System.Xml.XPath;
+using System.IO;
+using EAAutoFramework.Base;
+
+namespace EAAutoFramework.Config
+{
+    public class ConfigReader
+    {
+
+        public static void SetFrameworkSettings()
+        {
+
+            XPathItem aut;
+            XPathItem testtype;
+            XPathItem islog;
+            XPathItem isreport;
+            XPathItem buildname;
+            XPathItem logPath;
+            XPathItem appConnection;
+            XPathItem browsertype;
+
+            string strFilename = @"F:\UdemyEATestProject\EATestProject\EATestProject\Config\GlobalConfig.xml";
+            FileStream stream = new FileStream(strFilename, FileMode.Open);
+            XPathDocument document = new XPathDocument(stream);
+            XPathNavigator navigator = document.CreateNavigator();
+
+            //Get XML Details and pass it in XPathItem type variables
+            aut = navigator.SelectSingleNode("EAAutoFramework/RunSettings/AUT");
+            buildname = navigator.SelectSingleNode("EAAutoFramework/RunSettings/BuildName");
+            testtype = navigator.SelectSingleNode("EAAutoFramework/RunSettings/TestType");
+            islog = navigator.SelectSingleNode("EAAutoFramework/RunSettings/IsLog");
+            isreport = navigator.SelectSingleNode("EAAutoFramework/RunSettings/IsReport");
+            logPath = navigator.SelectSingleNode("EAAutoFramework/RunSettings/LogPath");
+            appConnection = navigator.SelectSingleNode("EAAutoFramework/RunSettings/ApplicationDb");
+            browsertype = navigator.SelectSingleNode("EAAutoFramework/RunSettings/Browser");
+
+            //Set XML Details in the property to be used accross framework
+            Settings.AUT = aut.Value.ToString();
+            Settings.BuildName = buildname.Value.ToString();
+            Settings.TestType = testtype.Value.ToString();
+            Settings.IsLog = islog.Value.ToString();
+            Settings.IsReporting = isreport.Value.ToString();
+            Settings.LogPath = logPath.Value.ToString();
+            Settings.AppConnectionString = appConnection.Value.ToString();
+            Settings.BrowserType = (BrowserType)Enum.Parse(typeof(BrowserType), browsertype.Value.ToString());
+
+
+        }
+
+    }
+}
